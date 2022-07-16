aadb208b0535f73a998110cda8fb89223198d9f7
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-16 17:21:34 +0100

    design a header

--- src/components/global/AppHeader.vue
+++ src/components/global/AppHeader.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="header">
-        <nav>
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link> |
-            <router-link to="/help">Help</router-link>
-        </nav>
+        <div class="logo">
+            Abdel<spna>.</spna>
+        </div>
+        <ul>
+            <router-link to="/" tag="li" exact>
+                <a>Home</a>
+            </router-link>
+            <router-link to="/about" tag="li">
+                <a>About</a>
+            </router-link>
+            <router-link to="/help" tag="li" replace>
+                <a>Help</a>
+            </router-link>
+        </ul>
     </div>
 </template>
 
@@ -13,3 +22,35 @@
         name: 'AppHeader',
     }
 </script>
+
+<style lang="scss" scoped>
+    .header{
+        background-color: #333;
+        color: #fff;
+        overflow: hidden;
+
+        .logo{
+            font-size: 30px;
+            width: 200px;
+            padding: 20px;
+            float: left;
+        }
+
+        ul{
+            float: right;
+            width: calc(100% - 200px);
+
+            li{
+                padding: 20px;
+                display: inline-block;
+                font-size: 18px;
+
+                a{
+                    color: white;
+                    text-decoration: none;
+                }
+            }
+        }
+    }
+</style>
+
