c39335f0aeb32b3544f6f4b9540633ed2d1003b0
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-17 00:20:42 +0100

    custome directives

--- src/components/blog/BlogSideBar.vue
+++ src/components/blog/BlogSideBar.vue
@@ -1,9 +1,19 @@
 <template>
-    <div class="sidebar-widgets">Test Sidebar</div>
+    <div v-font="90" class="sidebar-widgets">{{ text | uppercase }}</div>
 </template>
 
 <script>
 export default {
     name: "blogSidebar",
+    data: function () {
+        return {
+            text: "SideBar",
+        };
+    },
+    filters: {
+        uppercase: (v) => {
+            return v.toUpperCase();
+        },
+    },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -13,6 +13,13 @@ import "./filters";
 
 Vue.config.productionTip = false;
 
+//make custom directive to change font size
+Vue.directive("font", {
+    bind: (el, binding) => {
+        el.style.fontSize = binding.value + "px";
+    },
+});
+
 new Vue({
     router,
     store,
