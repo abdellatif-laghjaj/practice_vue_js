eda92e100c9ba204f7457ad257d71c97ac524ef7
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-16 19:38:03 +0100

    add bootsrap

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,12 @@
     <div class="about">
         <h1>{{ pageName }}</h1>
         <p>{{ pageDescription }}</p>
+        <div class="container">
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
     </div>
 </template>
 
