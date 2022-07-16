c0ae3ba0c5ec5e9085b872a9a43422274f53df7d
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-07-16 14:37:17 +0100

    add components

---
+++ src/components/WelcomeMessage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="welcome-message">
+        <h1>First vue cli project</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WelcomeMessage',
+}
+</script>
+
+<style scoped>
+h1 {
+    color: aqua;
+    font-size: 40px;
+}
+</style>
